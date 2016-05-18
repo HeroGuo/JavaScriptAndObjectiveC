c7929ef08b16ab2bbf66300cae749a26eafde5a3
guojunhong <jh_guo@tiertime.net>
2016-05-18 12:50:20 +0800

    first commit

---
+++ JavaScriptAndObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptAndObjectiveC
+//
+//  Created by GJH on 16/5/18.
+//  Copyright © 2016年 GJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
